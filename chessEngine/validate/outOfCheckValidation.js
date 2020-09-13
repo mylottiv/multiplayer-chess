18f7070365cd81c32f63d6dfdba358831b1c47a2
Thomas Mylott <mylottiv@gmail.com>
2020-09-13 16:09:44 -0500

    Second round validation that narrows valid movesets into moves that will remove king from check. Does NOT consider moves that would result in a check on the next turn, including moving king along the unblocked Path of a checking piece. That will be handled by the IntoCheck validation function

---
+++ chessEngine/validate/outOfCheckValidation.js
@@ -0,0 +1,12 @@
+function outOfCheckValidation(validPlayerMoves, opponentCheckingPieces) {
+    console.log('player', validPlayerMoves, 'opponent', opponentCheckingPieces);
+    const newValidPlayerMoves = validPlayerMoves.map(piece => {
+        let filterFunction;
+        if (piece.type === 'King') filterFunction = (coordinates) => !opponentCheckingPieces.some(({moveset}) => moveset.includes(coordinates))
+        else filterFunction = (playerMoveCoordinates) => opponentCheckingPieces.some(({coordinates, moveset}) => (opponentCheckingPieces.length === 1 && playerMoveCoordinates === coordinates) || moveset.includes(playerMoveCoordinates));
+        return {...piece, moveset: piece.moveset.filter(filterFunction)}
+    });
+    return newValidPlayerMoves;
+}
+
+module.exports = {outOfCheckValidation};
