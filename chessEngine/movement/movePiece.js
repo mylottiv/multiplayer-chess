c5667e55ec810a8e232155fcc2d8625160eef8c6
Thomas Mylott <mylottiv@gmail.com>
2020-09-16 18:43:57 -0500

    Added check for a castling move with rook movement handled through a recursive call

--- chessEngine/movement/movePiece.js
+++ chessEngine/movement/movePiece.js
@@ -4,7 +4,13 @@ function movePiece(chessboard, startingIndex, targetIndex) {
     const tempTargetState = chessboard[targetIndex].Piece;
     chessboard[startingIndex].Piece = null;
     chessboard[targetIndex].Piece = tempStartingState;
+    // Castling test
+    if ((tempStartingState.type === 'King') && (Math.abs(targetIndex - startingIndex) === 2)) {
+        const rookStartIndex = (targetIndex === 58) ? 56 : (targetIndex === 62) ? 63 : (targetIndex === 6) ? 7 : 0;
+        const rookTargetIndex = (rookStartIndex === 56) ? 59 : (rookStartIndex === 63) ? 61 : (rookStartIndex === 0) ? 3 : 5
+        movePiece(chessboard, rookStartIndex, rookTargetIndex);
+    }
     return tempTargetState;
-}
+};
 
 module.exports = {movePiece};
