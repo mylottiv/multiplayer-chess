8a29197ca65877d83ba6bca7d2b136410cf72ce6
Thomas Mylott <mylottiv@gmail.com>
2020-09-19 01:56:05 -0500

    Handles both the initial validations, and the final check and castling related validations, returning the final validated movesets of both players

---
+++ chessEngine/validateMoves.js
@@ -0,0 +1,26 @@
+const {initialValidation} = require('./initialValidation');
+const {checkValidate} = require('./finalValidation/checkValidate');
+const {outOfCheckValidation} = require('./finalValidation/outOfCheckValidation');
+const {castlingValidation} = require('./finalValidation/castlingValidation');
+const {checkMateTest} = require('./finalValidation/checkMateTest');
+
+function validateMoves(BoardState, playerColor) {
+
+    const opponentColor = (playerColor === 'White') ? 'Black' : 'White';
+
+    const initialValidatedMoves = initialValidation(BoardState)
+    const initialPlayerMoves = initialValidatedMoves[playerColor];
+    const initialOpponentMoves = initialValidatedMoves[opponentColor];
+
+    const {playerCheck: inCheck, checkingPieces} = checkValidate(initialPlayerMoves, initialOpponentMoves);
+    if (inCheck) {
+        const finalValidatedMoves = outOfCheckValidation(initialPlayerMoves, checkingPieces);
+        return (checkMateTest(finalValidatedMoves)) ? 'Checkmate' : finalValidatedMoves;
+    }
+    else {
+        const finalValidatedMoves = castlingValidation(BoardState, playerColor, initialPlayerMoves, initialOpponentMoves);
+        return finalValidatedMoves
+    }
+};
+
+module.exports = {validateMoves};
