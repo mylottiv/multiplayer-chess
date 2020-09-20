1de327d9a3546f618d74621b6c591c6b46a5fc80
Thomas Mylott <mylottiv@gmail.com>
2020-09-20 17:36:28 -0500

    Added validation logic for pawn promotion chars, refactored some tests as debugging required

---
+++ chessEngine/notationHandling/notationValidator.js
@@ -0,0 +1,35 @@
+const {notationParser} = require("./notationParser");
+const {startingPieceEnum, rankCharSet, fileCharSet} = require('../initialize/chessboardEnums');
+
+function notationValidator(rawNotation, playerColor) {
+
+    if (rawNotation === null || rawNotation.length < 2) return 'Invalid Notation';
+
+    const notationEndIndex = rawNotation.length - 1;
+
+    if (['O-O', 'O-O-O'].includes(rawNotation)) {
+        let targetNotation = (rawNotation === 'O-O') ? 'g' : 'c';
+        targetNotation += (playerColor === 'White') ? '1' : '8';
+        return {piece: 'King', targetNotation, startingNotation: (playerColor === 'White') ? '1' : '8'};
+    };
+
+    // Might need to step through from the end if a piece and/or '=' are detected
+    // console.log('firstTest', (!rankCharSet.includes(rawNotation[notationEndIndex]) && !startingPieceEnum.includes(rawNotation[notationEndIndex])));
+    if (!rankCharSet.includes(rawNotation[notationEndIndex]) && !startingPieceEnum.includes(rawNotation[notationEndIndex])) return 'Invalid Notation';
+
+    // console.log('secondTest', (!fileCharSet.includes(rawNotation[notationEndIndex - 1]) && rawNotation[notationEndIndex - 1] !== '='));
+    if (!fileCharSet.includes(rawNotation[notationEndIndex - 1]) && rawNotation[notationEndIndex - 1] !== '=') return 'Invalid Notation';
+
+    // console.log('thirdTest', (!(Object.keys(startingPieceEnum).includes(rawNotation[0]) || fileCharSet.includes(rawNotation[0]) || rankCharSet.includes(rawNotation[0]))));
+    if (!(Object.keys(startingPieceEnum).includes(rawNotation[0]) || fileCharSet.includes(rawNotation[0]) || rankCharSet.includes(rawNotation[0]))) return 'Invalid Notation';
+
+    // console.log('fourthTest', (!(fileCharSet.includes(rawNotation[1]) || rankCharSet.includes(rawNotation[1]) || rawNotation[1].toLocaleLowerCase() === 'x')))
+    if (!(fileCharSet.includes(rawNotation[1]) || rankCharSet.includes(rawNotation[1]) || rawNotation[1].toLocaleLowerCase() === 'x')) return 'Invalid Notation';
+    
+    // console.log('fifthTest', (rawNotation.length > 2 && !(fileCharSet.includes(rawNotation[2]) || rankCharSet.includes(rawNotation[2]) || rawNotation[0].toLocaleLowerCase() === 'x')))
+    if (rawNotation.length > 2 && !(fileCharSet.includes(rawNotation[2]) || rankCharSet.includes(rawNotation[2]) || rawNotation[0].toLocaleLowerCase() === 'x')) return 'Invalid Notation';
+
+    return notationParser(rawNotation, playerColor)
+}
+
+module.exports = {notationValidator};
