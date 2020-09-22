5a751d50d6730263cacefbd3d0b11fddeda5d240
Thomas Mylott <mylottiv@gmail.com>
2020-09-22 17:16:57 -0500

    Overall refactoring for clarity, replacing redundant stepper code with universalized stepperOperations functions

--- chessEngine/initialize/possibleMovesets/pathFinders.js
+++ chessEngine/initialize/possibleMovesets/pathFinders.js
@@ -1,47 +1,27 @@
 const {chessboardEdges} = require('../chessboardEnums');
+const {stepperOperations} = require('../stepperOperations');
 
 function straightPathFinder(index, direction) {
     if (chessboardEdges[direction].includes(index)) return [];
-    const indexOperations = {
-        left: (val) => val - 1,
-        right: (val) => val + 1,
-        top: (val) => val + 8,
-        bottom: (val) => val - 8,
-    };
-    let indexCounter = indexOperations[direction](index);
+    let indexCounter = stepperOperations[direction](index);
     const possibleIndexes = [];
     while (!chessboardEdges[direction].includes(indexCounter)) {
         possibleIndexes.push(indexCounter);
-        indexCounter = indexOperations[direction](indexCounter);
+        indexCounter = stepperOperations[direction](indexCounter);
     };
     // Add the edge index after while loop has been exited
     possibleIndexes.push(indexCounter)
     return possibleIndexes;
 };
 
-function diagonalPathFinder(index, horizontal, vertical) {
+function diagonalPathFinder(index, vertical, horizontal) {
     if (chessboardEdges[horizontal].includes(index) || chessboardEdges[vertical].includes(index)) return [];
-    const indexStepper = {
-        left: (val, vertical) => {
-            const vert = {
-                top: val + 7,
-                bottom: val - 9,
-            }
-            return vert[vertical]
-        },
-        right: (val, vertical) => {
-            const vert = {
-                top: val + 9,
-                bottom: val - 7
-            }
-            return vert[vertical]
-        }
-    };
-    let indexCounter = indexStepper[horizontal](index, vertical);
+    const diagonalDirection = vertical + '-' + horizontal;
+    let indexCounter = stepperOperations[diagonalDirection](index);
     const possibleIndexes = [];
     while (!chessboardEdges[horizontal].includes(indexCounter) && !chessboardEdges[vertical].includes(indexCounter)) {
         possibleIndexes.push(indexCounter);
-        indexCounter = indexStepper[horizontal](indexCounter, vertical);
+        indexCounter = stepperOperations[diagonalDirection](indexCounter);
     };
     // Add the edge index after while loop has been exited
     possibleIndexes.push(indexCounter);
@@ -49,31 +29,22 @@ function diagonalPathFinder(index, horizontal, vertical) {
 };
 
 function knightMovePairFinder(index, directionOne) {
-    const edgeChecker = (val, direction) => {
-        return chessboardEdges[direction].includes(val);
-    };
-    const directionStepReference = {
-        left: (val) => val - 1,
-        right: (val) => val + 1,
-        top: (val) => val + 8,
-        bottom: (val) => val - 8
-    };
-    const directionTwoPair = (directionOne === 'left' || directionOne === 'right') ? ['top', 'bottom'] : ['left', 'right'];
-    if (edgeChecker(index, directionOne)) return [];
-    let stepIndex = directionStepReference[directionOne](index);
-    if (edgeChecker(stepIndex, directionOne)) return [];
-    stepIndex = directionStepReference[directionOne](stepIndex);
+    const directionTwoPair = (['left', 'right'].includes(directionOne)) ? ['up', 'down'] : ['left', 'right'];
+    if (chessboardEdges[directionOne].includes(index)) return [];
+    let stepIndex = stepperOperations[directionOne](index);
+    if (chessboardEdges[directionOne].includes(stepIndex)) return [];
+    stepIndex = stepperOperations[directionOne](stepIndex);
     const pairArray = [];
-    let moveIndexOne = directionStepReference[directionTwoPair[0]](stepIndex);
-    if (!edgeChecker(stepIndex, directionTwoPair[0])) pairArray.push(moveIndexOne);
-    let moveIndexTwo = directionStepReference[directionTwoPair[1]](stepIndex);
-    if (!edgeChecker(stepIndex, directionTwoPair[1])) pairArray.push(moveIndexTwo);
+    let moveIndexOne = stepperOperations[directionTwoPair[0]](stepIndex);
+    if (!chessboardEdges[directionTwoPair[0]].includes(stepIndex)) pairArray.push(moveIndexOne);
+    let moveIndexTwo = stepperOperations[directionTwoPair[1]](stepIndex);
+    if (!chessboardEdges[directionTwoPair[1]].includes(stepIndex)) pairArray.push(moveIndexTwo);
     return pairArray;
 };
 
 function kingMoveFinder(index, direction) {
-    const topEdgeTest = chessboardEdges.top.includes(index);
-    const bottomEdgeTest = chessboardEdges.bottom.includes(index);
+    const topEdgeTest = chessboardEdges.up.includes(index);
+    const bottomEdgeTest = chessboardEdges.down.includes(index);
     const leftEdgeTest = chessboardEdges.left.includes(index);
     const rightEdgeTest = chessboardEdges.right.includes(index);
     const upLeftEdgeTest = (leftEdgeTest || topEdgeTest);
