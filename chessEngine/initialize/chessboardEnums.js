da84bd24f16235005a3996233fa6bc1c02c31f55
Thomas Mylott <mylottiv@gmail.com>
2020-09-22 17:11:43 -0500

    moved the rank and file range enums back to chessboardEnums and renamed the top and bottom edge keys to up and down for simplicity and consistency

--- chessEngine/initialize/chessboardEnums.js
+++ chessEngine/initialize/chessboardEnums.js
@@ -88,7 +88,7 @@ const chessboardEdges = {
         (() => chessboardNotationEnum['h7'])(),
         (() => chessboardNotationEnum['h8'])(),
     ],
-    top: [
+    up: [
         (() => chessboardNotationEnum['a8'])(),
         (() => chessboardNotationEnum['b8'])(),
         (() => chessboardNotationEnum['c8'])(),
@@ -98,7 +98,7 @@ const chessboardEdges = {
         (() => chessboardNotationEnum['g8'])(),
         (() => chessboardNotationEnum['h8'])(),
     ],
-    bottom: [
+    down: [
         (() => chessboardNotationEnum['a1'])(),
         (() => chessboardNotationEnum['b1'])(),
         (() => chessboardNotationEnum['c1'])(),
@@ -110,4 +110,26 @@ const chessboardEdges = {
     ],
 };
 
-module.exports = {chessboardNotationEnum, chessboardArrayEnum, chessboardEdges};
+const rankRangeEnum = {
+    1: [0, 7],
+    2: [8, 15],
+    3: [16, 23],
+    4: [24, 31],
+    5: [32, 39],
+    6: [40, 47],
+    7: [48, 55],
+    8: [56, 63],
+};
+
+const fileRangeEnum = {
+    a: [0, 56],
+    b: [1, 57],
+    c: [2, 58],
+    d: [3, 59],
+    e: [4, 60],
+    f: [5, 61],
+    g: [6, 62],
+    h: [7, 63]
+}
+
+module.exports = {chessboardNotationEnum, chessboardArrayEnum, chessboardEdges, fileRangeEnum, rankRangeEnum};
--- chessEngine/initialize/notationEnums.js
+++ chessEngine/initialize/notationEnums.js
@@ -10,26 +10,4 @@ const fileCharSet = ['a','b','c','d','e','f','g','h'];
 
 const rankCharSet = ['1','2','3','4','5','6','7','8'];
 
-const rankRangeEnum = {
-    1: [0, 7],
-    2: [8, 15],
-    3: [16, 23],
-    4: [24, 31],
-    5: [32, 39],
-    6: [40, 47],
-    7: [48, 55],
-    8: [56, 63],
-};
-
-const fileRangeEnum = {
-    a: [0, 56],
-    b: [1, 57],
-    c: [2, 58],
-    d: [3, 59],
-    e: [4, 60],
-    f: [5, 61],
-    g: [6, 62],
-    h: [7, 63]
-}
-
-module.exports = {startingPieceEnum, fileCharSet, rankCharSet, fileRangeEnum, rankRangeEnum}
+module.exports = {startingPieceEnum, fileCharSet, rankCharSet}
