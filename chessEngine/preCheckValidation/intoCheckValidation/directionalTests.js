305148f464091e8bf861a96ca84bc4a9fbd9b408
Thomas Mylott <mylottiv@gmail.com>
2020-09-24 19:19:04 -0500

    Expanded the edge tests to validate diagonal steps for being on the board edges

--- chessEngine/preCheckValidation/intoCheckValidation/directionalTests.js
+++ chessEngine/preCheckValidation/intoCheckValidation/directionalTests.js
@@ -30,14 +30,22 @@ function adjacencyTest(baseIndex, testIndex) {
         left: chessboardEdges.left.includes(baseIndex),
         right: !chessboardEdges.left.includes(baseIndex) && chessboardEdges.right.includes(baseIndex),
         up: chessboardEdges.up.includes(baseIndex),
-        down: !chessboardEdges.up.includes(baseIndex) && chessboardEdges.down.includes(baseIndex)
+        down: !chessboardEdges.up.includes(baseIndex) && chessboardEdges.down.includes(baseIndex),
+        'up-left': chessboardEdges.up.includes(baseIndex) || chessboardEdges.left.includes(baseIndex),
+        'up-right': chessboardEdges.up.includes(baseIndex) || chessboardEdges.right.includes(baseIndex),
+        'down-left': chessboardEdges.down.includes(baseIndex) || chessboardEdges.left.includes(baseIndex),
+        'down-right': chessboardEdges.down.includes(baseIndex) || chessboardEdges.right.includes(baseIndex),
     };
 
     const offBoardTests = [
         (edgeTests.left && signedDifference === -1),
         (edgeTests.right && signedDifference === 1),
         (edgeTests.up && signedDifference === 8),
-        (edgeTests.down && signedDifference === -8)
+        (edgeTests.down && signedDifference === -8),
+        (edgeTests["up-left"] && signedDifference === 7),
+        (edgeTests["up-right"] && signedDifference === 9),
+        (edgeTests["down-left"] && signedDifference === -9),
+        (edgeTests["down-right"] && signedDifference === -7)
     ]
 
     if (offBoardTests.includes(true)) return false;
