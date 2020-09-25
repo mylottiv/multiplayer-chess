a184f46717ea6ea956803f1a658a7156261ed99e
Thomas Mylott <mylottiv@gmail.com>
2020-09-24 20:53:13 -0500

    Refactoring

--- chessEngine/movement/movePiece.js
+++ chessEngine/movement/movePiece.js
@@ -1,44 +1,62 @@
 function movePiece(chessboard, startingIndex, targetIndex, promotionPiece) {
-    // console.log('movePiece', startingIndex, targetIndex);
+
     const startingPiece = chessboard[startingIndex].Piece;
     let capturedPiece = chessboard[targetIndex].Piece;
     chessboard[startingIndex].Piece = null;
     chessboard[targetIndex].Piece = startingPiece;
 
-    // Castling flag test
+    const absoluteDifference = Math.abs(targetIndex - startingIndex);
+
+    // Castling flag setter
     if (startingPiece.type === 'King' || startingPiece.type === 'Rook') {
+
         chessboard[targetIndex].Piece.canCastle = false;
+
     }
 
     // Castling move test
     if ((startingPiece.type === 'King') && (Math.abs(targetIndex - startingIndex) === 2)) {
-        const rookStartIndex = (targetIndex === 58) ? 56 : (targetIndex === 62) ? 63 : (targetIndex === 6) ? 7 : 0;
-        const rookTargetIndex = (rookStartIndex === 56) ? 59 : (rookStartIndex === 63) ? 61 : (rookStartIndex === 0) ? 3 : 5
+
+        const rookIndexes = {
+            58: [56, 59],
+            62: [63, 61],
+            6: [7, 5],
+            2: [0, 3]
+        };
+        const [rookStartIndex, rookTargetIndex] = rookIndexes[`${targetIndex}`];
         movePiece(chessboard, rookStartIndex, rookTargetIndex);
+
     }
 
     // En Passant flag test
-    if ((startingPiece.type === 'Pawn') && (Math.abs(targetIndex - startingIndex) === 16)) {
-        const enPassantRank = (startingPiece.color === 'White') ? [24, 31] : [32, 39];
-        const leftPiece = (targetIndex !== enPassantRank[0]) ? chessboard[targetIndex - 1].Piece : null; 
-        const rightPiece = (targetIndex !== enPassantRank[1]) ? chessboard[targetIndex + 1].Piece : null;
+    else if ((startingPiece.type === 'Pawn') && (absoluteDifference === 16)) {
+
+        const enPassantRankRange = (startingPiece.color === 'White') ? [24, 31] : [32, 39];
+
+        const leftPiece = (targetIndex !== enPassantRankRange[0]) ? chessboard[targetIndex - 1].Piece : null; 
+        const rightPiece = (targetIndex !== enPassantRankRange[1]) ? chessboard[targetIndex + 1].Piece : null;
         const leftPieceCheck = (leftPiece && leftPiece.type === 'Pawn' && leftPiece.color !== startingPiece.color);
         const rightPieceCheck = (rightPiece && rightPiece.type === 'Pawn' && rightPiece.color !== startingPiece.color);
-        chessboard[targetIndex].Piece.enPassant = (leftPieceCheck || rightPieceCheck) ? true : false;
+
+        chessboard[targetIndex].Piece.enPassant = (leftPieceCheck || rightPieceCheck);
+
     }
     
     // En Passant capture test
-    if ((startingPiece.type === 'Pawn') && ([7, 9].includes(Math.abs(targetIndex - startingIndex))) && (!capturedPiece)) {
+    else if ((startingPiece.type === 'Pawn') && ([7, 9].includes(absoluteDifference)) && (!capturedPiece)) {
+
         const capturedEnPassantPawnIndex = (startingPiece.color === 'White') ? targetIndex - 8 : targetIndex + 8;
         capturedPiece = chessboard[capturedEnPassantPawnIndex];
         chessboard[capturedEnPassantPawnIndex].Piece = null;
+
     }
 
     // Pawn Promotion test
-    if (startingPiece.type === 'Pawn' && promotionPiece !== null) {
-        const pawnColor = startingPiece.color;
-        chessboard[targetIndex].Piece = {type: promotionPiece, color: pawnColor};
+    else if (startingPiece.type === 'Pawn' && promotionPiece !== null) {
+
+        chessboard[targetIndex].Piece = {type: promotionPiece, color: startingPiece.color};
         if (promotionPiece === 'Rook') chessboard[targetIndex].Piece.canCastle = false;
+
     }
 
     return capturedPiece;
--- chessEngine/movement/validateParsedMove.js
+++ chessEngine/movement/validateParsedMove.js
@@ -1,27 +1,49 @@
 const {chessboardNotationEnum, fileRangeEnum, rankRangeEnum} = require('../constants/chessboardEnums');
-const {fileCharSet} = require('../constants/notationEnums');
+const {fileCharSet, rankCharSet} = require('../constants/notationEnums');
 const {movePiece} = require('./movePiece');
 
 function validateParsedMove(chessboard, validMoves, {piece, targetNotation, startingNotation, pawnPromotion}) {
+    
     const allValidOptions =  validMoves.filter(({type, moveset}) => (type === piece && moveset.includes(targetNotation)));
     // console.log('All Valid Options', allValidOptions);
     let selectedPieceNotation;
+    
     if (allValidOptions.length > 1) {
-        if (startingNotation === null) return 'Multiple possible moves please specify file or rank of moving piece'
+    
+        if (startingNotation === null) return 'Multiple possible moves please specify file or rank of moving piece';
+        
         else {
-            const startFileCheck = (fileCharSet.includes(startingNotation));
-            const startingNotationRange = startFileCheck ? fileRangeEnum[startingNotation] : rankRangeEnum[startingNotation];
+            const startFileOrRank = (fileCharSet.includes(startingNotation)) 
+                ? 'File' 
+                : (rankCharSet.includes(startingNotationRange)) 
+                    ? 'Rank' 
+                    : 'None';
+            const startingRangeRef = {
+                File: (notation) => fileRangeEnum[notation],
+                Rank: (notation) => rankRangeEnum[notation],
+                None: () => 'None'
+            }
+            const startingNotationRange = startingRangeRef[startFileOrRank](startingNotation);
             selectedPieceNotation = allValidOptions.find(({type, coordinates}) => {
                 const coordinateIndex = chessboardNotationEnum[coordinates];
-                const test = (startFileCheck) ? (diff) => diff % 8 === 0 : (diff) => diff <= 7
-                const inRange = startingNotationRange.every(rangeIndex => test(Math.abs(coordinateIndex - rangeIndex)))
-                return (type === piece) && inRange; 
+                const differenceRef = {
+                    File: (diff) => (diff % 8 === 0),
+                    Rank: (diff) => (diff <= 7),
+                    None: 'None'
+                };
+                const differenceTest = differenceRef[startFileOrRank];
+                const inRange = (differenceTest !== 'None') 
+                    ? startingNotationRange.every(rangeIndex => differenceTest(Math.abs(coordinateIndex - rangeIndex)))
+                    : 'None';
+                return (type === piece) && (inRange || inRange === 'None'); 
             }).coordinates;
+        
         };
+    
     }
-    else {
-        selectedPieceNotation = allValidOptions[0].coordinates;
-    }
+
+    else selectedPieceNotation = allValidOptions[0].coordinates;
+
     return movePiece(chessboard, chessboardNotationEnum[selectedPieceNotation], chessboardNotationEnum[targetNotation], pawnPromotion);
 }
 
