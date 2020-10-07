8b58d0f2bba866e976d17978c7ff7f301426aa82
Thomas Mylott <mylottiv@gmail.com>
2020-10-07 14:46:02 -0500

    Temporary state management to test chessboard state inputs

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ChessboardBackground from '../components/ChessboardBackground';
 import SquareGrid from '../components/SquareGrid';
 import {rankRangeEnum} from '../chessEngine/constants/chessboardEnums';
 import {newBoard} from '../chessEngine/constants/defaultBoardState';
+import {readInBoardState} from  '../testReadInBoardState';
 import tw from 'twin.macro';
 
-export default function Index({styling, squareRankRef, board}){
+export default function Index({styling, squareRankRef, btnStyling, startingBoard, boardStore}){
+
+    const [boardStateStore, setBoardStateStore] = useState(boardStore)
+    const [currentBoardState, setCurrentBoardState] = useState(startingBoard);
+    const [currentTurn, setCurrentTurn] = useState(-1);
+    const [currentColor, setCurrentColor] = useState('Starting');
+    const incrementBoardState = (currentTurn, currentColor) => {
+        if (currentColor === 'WhiteTurn') setCurrentColor('BlackTurn');
+        else {
+            setCurrentColor('WhiteTurn');
+            setCurrentTurn(currentTurn + 1);
+        }
+    };
+
+    useEffect(
+        () => {
+            currentColor !== 'Starting' && setCurrentBoardState(boardStateStore[currentTurn][currentColor]);
+        },
+        [currentTurn, currentColor, boardStateStore]
+    )
+
     return (
+        <>
         <div css={styling}>
-            <SquareGrid rangeRef={squareRankRef} boardState={board}/>
+            <SquareGrid rangeRef={squareRankRef} boardState={currentBoardState}/>
             <ChessboardBackground />
         </div>
+        <button css={btnStyling} onClick={() => incrementBoardState(currentTurn, currentColor)}>Next Board T: {currentTurn + 1} C: {currentColor}</button>
+        </>
     )
 }
 
@@ -21,8 +45,12 @@ export async function getStaticProps() {
                 tw`relative mx-4 mt-8 mb-4`,
                 `width: 330px; height: 330px`
             ],
+            btnStyling: [
+                tw`bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded`
+            ],
             squareRankRef: rankRangeEnum,
-            board: newBoard()
+            startingBoard: newBoard(),
+            boardStore: readInBoardState('./localDummyData/dummyCheckmateOutputs.txt')
         }
     }
 };
