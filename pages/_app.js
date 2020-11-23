05cb99d890209a2cefeb9467f6425f159c3777dc
Thomas Mylott <mylottiv@gmail.com>
2020-11-23 16:45:25 -0600

    Implemented WS link and Client Link Splitter

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,44 @@ import '../styles/index.css';
 // import { GlobalStyles } from 'twin.macro';
 // Or, sans animate classes
 // import 'tailwindcss/dist/base.min.css'
+import { ApolloClient, ApolloProvider, InMemoryCache, split, HttpLink} from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { WebSocketLink } from '@apollo/client/link/ws';
+
+const httpLink = new HttpLink({
+    uri: 'http://localhost:3000/api/graphql'
+  });
+  
+
+const wsLink = process.browser ? new WebSocketLink({
+    uri: `ws://localhost:3000/api/graphql`,
+    options: {
+      reconnect: true
+    }
+  }) : null;
+
+  const splitLink = process.browser ? split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription'
+      );
+    },
+    wsLink,
+    httpLink,
+  ) : httpLink;
+
+const client = new ApolloClient({
+    link: splitLink,
+    cache: new InMemoryCache()
+});
+
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <ApolloProvider client={client}>
+            <Component {...pageProps} />
+        </ApolloProvider>
+    )
 }
